d3345a34294a5e2995e520f0d487962923bbc3ed
amandabarth <112357592+amandabarth@users.noreply.github.com>
2022-12-04 12:46:03 -0500

    Final Project: worked on the Prices page

---
+++ prices.php
@@ -0,0 +1,50 @@
+<?php include 'top.php'; ?>
+<main>
+            <h1>Prices</h1>
+            <section> 
+                <h2>Prices starting at...</h2>
+                <table>
+                        <tr>
+                            <th>Type</th>
+                            <th>Price per Reading</th>
+                            
+                        </tr>
+                        <tr>
+                            <td>Individual</td>
+                            <td>$10</td>
+                            
+                        </tr>
+                        <tr>
+
+                        </tr>
+                        <tr>
+                            <td>Couples</td>
+                            <td>$15</td>
+                            
+                        </tr>
+                        <tr>
+                            <td>Group</td>
+                            <td>$15 + $10 per person </td>
+                            
+                        </tr>
+                </table>
+                
+            </section>
+            <section> 
+                <h2>About Each</h2>
+                <h3>Individual</h3>
+                <p>Are you wondering what your future holds? Or perhaps you are looking for insight into your inner psyche... Either way, an individual reading will give you the answers and insight you need to guide you on your journey. Come to the reading with intentions and know what you hope to get out of it. This is your session so take the time to look deep within yourself to get the most out of it.</p>
+                <h3>Couples</h3>
+                <p>Relationships can be complicated, but couple's tarot readings can help clear the air. Couples tarot readings are intuitive readings that delve into fibers of your relationship. How do you love eachother? What does your future hold? Is your relationship strong enough to stand against the river of time or will your relationship be swept away?</p>
+                <h3>Group</h3>
+                <p>Group tarot readings are a fun way to delve into the relationships you have with your friends. Group dinamics can be complicated, but a tarot reading will certainly may help you sort out some of the knots in your web of friendships. Although you must be wary of these types of tarot readings as emotions and past conflicts may arise. Once the reading is over, you should have clarity and know what everyone wants out of the friendships and feel more connected to one another.</p>
+            </section>
+            <section> 
+                <h2>Other</h2>
+                <p>Readings are Boulder and Burlington based. All readings are also available online from anywhere. Each reading is 30 minutes minimum, and then theres an aditional cost for readings over 30 minutes.</p>
+            </section>
+        </main>
+        <?php
+        include 'footer.php';
+        ?>
+    </body>
