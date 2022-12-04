cec99b0e44fba34aa6c707e5f7e9cdbc3ca68003
amandabarth <112357592+amandabarth@users.noreply.github.com>
2022-12-04 17:02:07 -0500

    Final Project: Update Footer

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
     <footer> 
         <p><a href="../index.php">Site map</a></p>
-        <p><a href='/acbarth.w3.uvm.edu'>Amanada Barth</a></p>
+        <p><a href='acbarth.w3.uvm.edu/cs008/final'>Amanada Barth</a></p>
         <p><a href='rlkoenig.w3.uvm.edu/cs008/final'>Rachel Koenig</a></p>
-        <p><a href="gschutz.w3.uvm.edu">Greta Schutz</a></p>
+        <p><a href="gschutz.w3.uvm.edu/cs008/final">Greta Schutz</a></p>
     </footer>
 </body>
 </html>
