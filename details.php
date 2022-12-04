d3113754810bc3c1b101a06b33150dee541c4775
amandabarth <112357592+amandabarth@users.noreply.github.com>
2022-12-04 12:45:38 -0500

    Final Project: worked on the detail page

--- details.php
+++ details.php
@@ -2,21 +2,48 @@
 ?>
         <main>
             <h1>More Information</h1>
-            <section class="flexA">
-                <h2></h2>
-                <figure class="image">
-                    <img src="images/polarbear630x421.jpeg" alt="Polar Bear standing on a floating piece of ice">
-                    <figcaption>Polar Bears habitats are melting which is endangering their lives.<cite>https://www.huffpost.com/entry/runaway-climate-change-2030-report_n_5b8ecba3e4b0162f4727a09f</cite></figcaption>
+            <section class="grid-layout">
+            <section class="sec-1">
+                <h2>Facts about Tarot</h2>
+                <p>Tarot cards can help one to receive important messages and perceive their current situations in a different light. Each card represents a different lesson or situation. When shuffling the cards, you can shuffle the seventy eight cards in whatever way you would like. What matters while shuffling is putting your intentions for the session into the cards. There are five different groupings of cards. The major arcana, the wands, the cups, the swords, the pentacles. The major arcana contains a large number of cards that are all different, where the other four suits are similar to playing cards with numbers, and then a page, knight, queen, and king. Each card has an individual meaning, however it is up to the interpretation of the person doing your reading. The major arcana is meant to represent long term feelings and situations, while the other suits can be just as influential in the short term. There are three meanings behind most cards, the upright meaning, the reversed meaning, and the advice position. Keep scrolling to see what some of the cards mean. <cite>https://www.tarot.com/tarot/cards</cite></p>
+            </section>
+            <section class="sec-2">
+                <figure class = "image">
+                    <img src="images/tarot-cards.jpg" alt="Tarot Cards Scattered in Grass">
                 </figure>
-                <p></p>
+                <p>Fun Fact: Tarot began in Europe in the fourteenth century.</p>
             </section>
-            <section class="flexB">
-                <h2></h2>
-                <p></p>
+            
+            <h2 class ="sec-2-1"> Examples of Upright Card Meanings</h2>
+            
+            <section class ="sec-3">
+        
+                <figure class="image">
+                    <img src="images/three-of-swords-tarot-card.jpg" alt="Three of Swords Tarot Card">
+                </figure>
+                <p>	Three of swords means that one has gone through or is about to go through a breakup of some sort. It could also mean that you need to rethink your current relationships that are causing turmoil.</p>
             </section>
-            <section class="flexC">
-                <h2></h2>
-                <p></p>
+
+            <section class ="sec-4">
+                <figure class="image">
+                    <img src="images/the-chariot-tarot-card.jpg" alt="The Chariot Tarot Card">
+                </figure>
+                <p>The Chariot represents a feeling of freedom and when drawn it is meant to make one feel empowered.</p>
+            </section>
+
+            <section class ="sec-4">
+                <figure class="image">
+                    <img src="images/queen-of-wands-tarot-card.jpg" alt="The Queen of Wands Tarot Card">
+                </figure>
+                <p>Queen of Wands points to strong ambition and being a natural born leader like a queen. One is a risk taker and will do whatever it takes to succeed.</p>
+            </section>
+
+            <section class ="sec-5">
+                <figure class="image">
+                    <img src="images/the-fool-tarot-card.jpg" alt="The Fool Tarot Card">
+                </figure>
+                <p>The Fool is an entertainer and represents vulnerability. This card helps one to realize the path they need to take and reach self-realization.</p>
+            </section>
             </section>
         </main>
         <?php
