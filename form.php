414f528926d3e6d18c093cfcd8835c24779fe5d0
rachellkoenig <112489533+rachellkoenig@users.noreply.github.com>
2022-12-04 12:45:50 -0500

    form.php

--- form.php
+++ form.php
@@ -1,12 +1,12 @@
 <?php include 'top.php';
 $dataIsGood = true;
 $message = '';
+$mailMessage = '';
+$mailSent = '';
 $email = '';
-$question = '';
-$termsAndService = false;
-$emailList = true;
-$postOnSite = true;
-$knowledge = 'YES!';
+$name = '';
+$appointTime = '';
+$type = 'Individual';
 
 function sanatize($field){
     if(!isset($_POST[$field])){
@@ -20,9 +20,9 @@ function sanatize($field){
 }
 ?>
         <main>
-            <h1>Questions</h1>
+            <h1>Schedule a Reading</h1>
             <section class="flexA">
-                <h2>Please submit this form if you have any questions!</h2>
+                <h2>To schedule a reading, submit this form!</h2>
 <?php
     if($_SERVER["REQUEST_METHOD"] == 'POST'){
     $email = sanatize("txtEmail");
@@ -31,36 +31,50 @@ function sanatize($field){
         $dataIsGood = false;
     }
 
-    $question = sanatize("txtQuestion");
+    $name = sanatize("txtName");
 
-    $termsAndService = (int) sanatize('chkTermsAndService');
-    $emailList = (int) sanatize('chkEmailList');
-    $postOnSite = (int) sanatize('chkPostOnSite');
-    if($termsAndService != 1){
-        print '<p class="wrong">Please agree to Terms of Service. </p>';
-        $dataIsGood = false;
-    }
+    $appointTime = sanatize('type');
 
-    $knowledge = sanatize("newInfo");
-    if($knowledge != "YES!" AND $knowledge != "some" AND $knowledge != "no"){
-        print '<p class="wrong">Please choose if you learned or not. </p>';
+    $type = sanatize("newInfo");
+    if($type != "Individual" AND $type != "Couple" AND $type != "Group"){
+        print '<p class="wrong">Please chose which type of reading you would like. For more information on what each reading includes go to the pricing page.</p>';
         $dataIsGood = false;
     }
 
     if($dataIsGood){
-        $sql = 'INSERT INTO tblQuestions(fldEmail, fldQuestion, fldTermsOfService, fldEmailList, fldPostOnSite, fldKnowledge) VALUES(?,?,?,?,?,?)';
+        $sql = 'INSERT INTO tblSchedule(fldEmail, fldName, fldAppointTime, fldType) VALUES(?,?,?,?,?,?)';
         $statement = $pdo->prepare($sql);
-        $data = array($email, $question, $termsAndService, $emailList, $postOnSite, $knowledge);
+        $data = array($email, $name, $appointTime, $type);
 
         if($statement->execute($data)){
-            $message = '<h2>Thank you</h2>';
-            $message .= '<p>Your question was sucessfully sent!</p>';
+            print '<p>Your reading was sucessfully scheduled!</p>';
+
+            $to = $email;
+            $from = 'CS 008 Team <rlkoenig@uvm.edu>';
+            $subject = 'You Scheduled a Tarot Reading';
+
+            $mailMessage = '<p style="font:">Thank you' . $name . 'for scheduling a reading! Your appointment is at '. $appointTime. '</p>';
+            $mailMessage .= '<p>See you then!</p>';
+            $mailMessage .= '<p>Linsey Reads Tarot</p>';
+
+            $headers = "MIME_Version: 1.0\r\n";
+            $headers .= "Content-type: text/html; charset=stf-8\r\n";
+            $headers .= "From: ".$from. "\r\n";
+
+            $mailSent = mail($to, $subject, $mailMessage, $headers);
+
+            if ($mailSent){
+                print "<p>A copy has been emailed to your records.</p>";
+                print $mailMessage; 
+            }
+
+        } else {
+            print '<p>Your reading was NOT scheduled.<p>';
         }
-        else {
-            $message = '<p class="wrong">Your question was not submitted.<p>';
-        }
-    } 
+     
+    }
 }
+    
 ?>
                 <form action="#" method="POST">
                     <fieldset>
@@ -69,36 +83,28 @@ function sanatize($field){
                             <input type="text" name="txtEmail" id="txtEmail" value = "<?php print $email; ?>" required>
                         </p>
                         <p class="form">
-                            <label for="txtQuestion">Questions:</label>
-                            <textarea name="txtQuestion" id="txtQuestion" placeholder="?" rows="3" cols="20"><?php print $question; ?></textarea>
+                            <label for="txtName">Name:</label>
+                            <input type = "text" name="txtName" id="txtName" value="<?php print $name; ?>">
                         </p>
                     </fieldset>
                     <fieldset>
                         <p class="form">
-                            <input type="checkbox" name="chkTermsAndService" id="chkTermsAndService" value="1" <?php if ($termsOfService) print 'checked';?>>
-                            <label for="chkTermsAndService">Agree to terms of service</label>
-                        </p>
-                        <p class="form">
-                            <input type="checkbox" name="chkEmailList" id="chkEmailList" value="1" <?php if ($emailList) print 'checked';?>>
-                            <label for="chkEmailList">Sign up for Email List</label>
-                        </p>
-                        <p class="form">
-                            <input type="checkbox" name="chkPostOnSite" id="chkPostOnSite" value="1" <?php if ($postOnSite) print 'checked';?>>
-                            <label for="chkPostOnSite">Agree to your question being posted</label>
+                            <label for="appointTime">When would you like to schedule your reading for?</label>
+                            <input type="datetime-local" name="appointTime" id="appointTime" value="<?php print $appointTime;?>">
                         </p>
                     </fieldset>
                     <fieldset>
-                        <legend>Survey: Did you Learn Something?</legend>
+                        <legend>What type of reading would you like?</legend>
                         <p class="form">
                             
-                            <input type="radio" name="newInfo" id="radYes" value="YES!" <?php if ($knowledge == 'YES!') print 'checked';?>> 
-                            <label for="radYes">YES!</label>&nbsp;
+                            <input type="radio" name="type" id="radIndiv" value="Individual" <?php if ($knowledge == 'Individual') print 'checked';?>> 
+                            <label for="radIndiv">Individual</label>&nbsp;
                             
-                            <input type="radio" name="newInfo" id="radSome" value="some" <?php if ($knowledge == 'some') print 'checked';?>> 
-                            <label for="radSome">A Little Bit</label>&nbsp;
+                            <input type="radio" name="type" id="radCouple" value="Couple" <?php if ($knowledge == 'Couple') print 'checked';?>> 
+                            <label for="radCouple">Couple</label>&nbsp;
                             
-                            <input type="radio" name="newInfo" id="radNo" value="no" <?php if ($knowledge == 'no') print 'checked';?>> 
-                            <label for="radNo">No :(</label>&nbsp;
+                            <input type="radio" name="type" id="radGroup" value="Group" <?php if ($knowledge == 'Group') print 'checked';?>> 
+                            <label for="radGroup">Group</label>&nbsp;
                         </p>
                     </fieldset>
                     <fieldset>
@@ -109,7 +115,7 @@ function sanatize($field){
                 </form>
         </section>
         <section class="flexB">
-            <h2>We value your response!</h2>
+            <h2>Check your email </h2>
                 <?php
                     print $message;
                     print '<p>Post Array:</p><pre>';
@@ -118,19 +124,11 @@ function sanatize($field){
                 ?>
             </section>
             <section class="flexC">
-                <h2>Thank you to everyone who submitted questions!</h2>
+                <h2>Thank you for Scheduling an appointment!</h2>
                 <figure class="image">
                     <img src="images/thank_you_small.jpg" srcset="images/thank_you_medium.jpg 2x, images/thank_you.jpg 3x" alt="Thank You">
                     <figcaption><cite>https://emilypost.com/advice/different-ways-to-say-thank-you</cite></figcaption>
                 </figure>
-            </section>
-            <section class="flexB">
-                <h2>Questions:</h2>
-                <p class="form">#1 Is climate change real?</p>
-            </section>
-            <section class="flexC">
-                <h2>Answers:</h2>
-                <p class="form">#1 Yes, but we have the power to stop it from continuing at such a drastic rate.</p>
             </section>
         </main>
         <?php
