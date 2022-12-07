755d44bd28e1485e460cd1f6da5d80585b0da8a5
amandabarth <112357592+amandabarth@users.noreply.github.com>
2022-12-07 13:04:58 -0500

    Final Project: footer with working links

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
     <footer> 
         <p><a href="../index.php">Site map</a></p>
-        <p><a href='acbarth.w3.uvm.edu/cs008/final'>Amanada Barth</a></p>
-        <p><a href='rlkoenig.w3.uvm.edu/cs008/final'>Rachel Koenig</a></p>
-        <p><a href="gschutz.w3.uvm.edu/cs008/final">Greta Schutz</a></p>
+        <p><a href='https://acbarth.w3.uvm.edu/cs008/'>Amanada Barth</a></p>
+        <p><a href='https://rlkoenig.w3.uvm.edu/cs008/'>Rachel Koenig</a></p>
+        <p><a href="https://geschutz.w3.uvm.edu/cs008/">Greta Schutz</a></p>
     </footer>
 </body>
 </html>
